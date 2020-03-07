e12d5336e702a5f62f565fa437de38a9e8c2a01b
Matt Leuer <mattleuer@gmail.com>
2020-03-07 16:56:31 -0700

    High scores are stored in local storage.

--- Develop/script.js
+++ Develop/script.js
@@ -1,83 +1,138 @@
-var question1 = {
-    name: "Which of the following is not a primitive?",
-    possibleAnswers: ["Number", "Object", "Boolean", "String"],
-    correctAnswer: "Object"
+class Question {
+    constructor(description, possibleAnswers, correctAnswer) {
+        this.description = description;
+        this.possibleAnswers = possibleAnswers;
+        this.correctAnswer = correctAnswer;
+    }
 }
 
-var question2 = {
-    name: "Code contained within '{}' is known as a...? ",
-    possibleAnswers: ["Block", "Statement", "Condition", "Loop"],
-    correctAnswer: "Block"
-}
-
-var question3 = {
-    name: "For loops are mainly used to iterate through...?",
-    possibleAnswers: ["Objects", "Functions", "Numbers", "Collections"],
-    correctAnswer: "Collections"
-}
-
-var question4 = {
-    name: "A useful method to use during debugging is...?",
-    possibleAnswers: ["Console.log()", "Array.foreach()", "alert()", "prompt()"],
-    correctAnswer: "Console.log()"
-}
-
-var question5 = {
-    name: "'count += 1;' can be rewritten as...",
-    possibleAnswers: ["count = 1;", "count = count + 1;", "var count = 1;", "var count = count + 1;"],
-    correctAnswer: "count = count + 1;"
-}
+var question1 = new Question("Which of the following is not a primitive?", ["Number", "Object", "Boolean", "String"], "Object");
+var question2 = new Question("Code contained within '{}' is known as a...? ", ["Block", "Statement", "Condition", "Loop"], "Block");
+var question3 = new Question("For loops are mainly used to iterate through...?", ["Objects", "Functions", "Numbers", "Collections"], "Collections");
+var question4 = new Question("A useful method during debugging is...?", ["Console.log()", "Array.foreach()", "alert()", "prompt()"], "Console.log()");
+var question5 = new Question("'count += 1;' can be rewritten as...", ["count = 1;", "count = count + 1;", "var count = 1;", "var count = count + 1;"], "count = count + 1;");
 
 var questions = [question1, question2, question3, question4, question5];
+var questionToBeRendered = 0;
+var questionBeingRendered;
+var seconds = 50;
+var score = 0;
 
-var headerElement = document.querySelector("#main-header");
-var descriptionElement = document.querySelector("#description");
-var responseElement = document.querySelector("#response");
+var welcomeCard = document.querySelector(".welcome-card");
+var questionCard = document.querySelector(".question-card");
+var responseCard = document.querySelector(".response-card");
+var highScoreCard = document.querySelector(".highscore-card");
+
 var startButton = document.querySelector("#start-button");
+var saveButton = document.querySelector("#save-button");
+var userInputField = document.querySelector("#user-input");
+
+var questionDescription = document.querySelector("#question-description");
 var button1 = document.querySelector("#question1-button");
 var button2 = document.querySelector("#question2-button");
 var button3 = document.querySelector("#question3-button");
 var button4 = document.querySelector("#question4-button");
+
+var responseElement = document.querySelector("#response");
+
+var scoreEl = document.querySelector("#score");
 var highscoreButton = document.querySelector("#highscore");
 var timerElement = document.querySelector("#timer-count");
 var questionList = document.querySelector("#question-list");
 
 var buttons = [button1, button2, button3, button4];
 
+
 startButton.addEventListener("click", function () {
-    descriptionElement.setAttribute("style", "display: none");
-    startButton.setAttribute("style", "display: none");
-    var seconds = 75;
-    setInterval(function () {
+    var timer = setInterval(function () {
         seconds--;
         timerElement.textContent = seconds;
+        if (seconds <= 0) {
+            renderGameOver();
+            timerElement.textContent = seconds;
+            clearInterval(timer);
+        }
     }, 1000);
-    presentQuestion(1);
+
+    renderQuestion(questionToBeRendered);
 });
 
-button1.addEventListener("click", function() {
-
+button1.addEventListener("click", function () {
+    responseElement.setAttribute("style", "display: unset");
+    checkAnswer(this);
 });
 
-button2.addEventListener("click", function() {
+button2.addEventListener("click", function () {
+    responseElement.setAttribute("style", "display: unset");
+    checkAnswer(this);
+});
+
+button3.addEventListener("click", function () {
+    responseElement.setAttribute("style", "display: unset");
+    checkAnswer(this);
+});
+
+button4.addEventListener("click", function () {
+    responseElement.setAttribute("style", "display: unset");
+    checkAnswer(this);
+});
+
+saveButton.addEventListener("click", function() {
+    var scores = {};
+    if(localStorage.getItem("scores")) {
+        scores = JSON.parse(localStorage.getItem("scores"));
+    }
+    Object.defineProperty(scores, userInputField.value, {value: score, enumerable: true});
+
+    var scoresJSON = JSON.stringify(scores);
+    localStorage.setItem("scores", scoresJSON);
+    window.location.href = "./highscore.html";
+})
+
+function getHighScores() {
     
-});
+}
 
-button3.addEventListener("click", function() {
-    
-});
+function renderQuestion(questionNumber) {
+    welcomeCard.setAttribute("style", "display: none");
+    responseCard.setAttribute("style", "display: none");
 
-button4.addEventListener("click", function() {
-    
-});
+    var question = questions[questionNumber];
+    questionBeingRendered = question;
+    questionDescription.textContent = questionNumber + 1 + ": " + question.description;
 
-function presentQuestion(questionNumber) {
-    var question = questions[questionNumber]
-    headerElement.textContent = questionNumber + ": " + question.name;
-    
-    for(var i = 0; i < question.possibleAnswers.length; i++) {
+    questionCard.setAttribute("style", "display: initial");
+
+
+    for (var i = 0; i < question.possibleAnswers.length; i++) {
         buttons[i].textContent = question.possibleAnswers[i];
         buttons[i].setAttribute("style", "display: unset");
     }
+    questionToBeRendered++;
+}
+
+function checkAnswer(button) {
+    if (button.textContent !== questionBeingRendered.correctAnswer) {
+        responseElement.textContent = "Incorrect";
+        seconds -= 10;
+    }
+    else {
+        responseElement.textContent = "Correct";
+        score += 5;
+    }
+    setTimeout(function () {
+        if (questionToBeRendered < questions.length) {
+            renderQuestion(questionToBeRendered);
+        }
+        else {
+            renderGameOver();
+        }
+    }, 500);
+}
+
+function renderGameOver() {
+    questionCard.setAttribute("style", "display: none");
+    highScoreCard.setAttribute("style", "display: unset");
+    scoreEl.textContent = "Your Score: " + score;
 }
 
